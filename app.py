836d5453108435050cb83ee0bebf6fcffe14eb2a
AmadUdDinGakkhar <a_gakkhar@hotmail.com>
2024-08-10 12:14:15 +0500

    added fast api code

--- app.py
+++ app.py
@@ -3,10 +3,38 @@ from heart_disease_prediction.pipline.prediction_pipeline import PredictionPipel
 from heart_disease_prediction.constants import BEST_MODEL_PATH, PREPROCESSOR_PATH
 from dotenv import load_dotenv
 import pandas as pd
+from fastapi import FastAPI, Response
+from fastapi.responses import HTMLResponse
 
 load_dotenv()
 
 test_df = pd.read_csv("/home/amadgakkhar/code/MLOps-Project/sample_test.csv")
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the app!"}
+
+
+@app.get("/train")
+def train_pipeline():
+    TrainPipeline().start_training()
+    return {"message": "Training Completed"}
+
+
+# @app.post("/test")
+# def test_pipeline(test_data: pd.DataFrame):
+#     estimate = PredictionPipeline(test_data).predict()
+#     print(estimate)
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
 
 # TrainPipeline().start_training()
@@ -17,5 +45,5 @@ test_df = pd.read_csv("/home/amadgakkhar/code/MLOps-Project/sample_test.csv")
 #     best_model_path=BEST_MODEL_PATH,
 # ).run()
 
-estimate = PredictionPipeline(1).predict()
-print(estimate)
+# estimate = PredictionPipeline(1).predict()
+# print(estimate)
